c0e2c1bf3518001b931d1cf514b55753168cb57c
Pedro Athayde <pedro_athayde@outlook.com>
2024-11-25 17:49:37 -0300

    feat: Added lazy loading for beans

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/curso/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.curso.springcoredemo.rest;
+
+import com.curso.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //define private field for dependecy
+    private Coach myCoach;
+
+    //define a constructor
+
+    @Autowired
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
